49b77b15f6efc1f4222b66fa1ce3549ada249b7e
christijohnson <christiajohnson@gmail.com>
2024-01-18 19:24:23 -0700

    Update L2-team code

--- routes/index.js
+++
@@ -1,7 +0,0 @@
-const routes = require('express').Router();
-const userController = require('../controllers/user');
-
-routes.get('/', userController.getUser);
-routes.get('/username', userController.getUsername);
-
-module.exports = routes;
